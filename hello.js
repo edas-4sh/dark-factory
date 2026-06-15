e3dfec7e58807e95427a99e56c19c0113ee03cea
edas-4sh <darkfactoryash@gmail.com>
2026-06-15 19:00:02 +0530

    Add hello.js script that prints 'Hello World'

--- hello.js
+++ hello.js
@@ -1,2 +1,6 @@
-// Simple script that prints Hello World to the console
+// hello.js
+// Simple script that prints 'Hello World' to the console.
+
+// When executed with Node.js, this will output the greeting.
+
 console.log('Hello World');
